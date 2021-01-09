6ef258f9b7d9ed3c7780883932d36b301908386b
nelo <nelokwaz@gmail.com>
2021-01-09 09:42:05 +0100

    added the basic layout structure of the

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,7 +1,15 @@
 import React from "react";
 
 function Login() {
-  return <div>Login</div>;
+  return (
+    <div>
+      <h2>Login</h2>
+      <form method="post">
+        <input type="text" placeholder="Input username" />
+        <button type="submit">Submit</button>
+      </form>
+    </div>
+  );
 }
 
 export default Login;
