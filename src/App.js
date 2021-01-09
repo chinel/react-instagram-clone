31933907f6a370cc562805c64ac9ebfb21196cc7
nelo <nelokwaz@gmail.com>
2021-01-09 09:35:28 +0100

    added basic conditional to show login page is user is not logged in

--- src/App.js
+++ src/App.js
@@ -1,5 +1,12 @@
 import React from "react";
+import Login from "./components/Login";
 function App() {
+  let user = "";
+
+  if (!user) {
+    return <Login />;
+  }
+
   return <div>app</div>;
 }
 
