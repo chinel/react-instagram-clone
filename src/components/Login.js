e8e0863fa5f9613c1a5fa787ce005dba092e2193
nelo <nelokwaz@gmail.com>
2021-01-09 10:23:18 +0100

    created a basic logic to handle login data on submit

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,11 +1,21 @@
 import React from "react";
 
 function Login() {
+  const [username, setUsername] = React.useState("");
+
+  function handleSubmit(event) {
+    event.preventDefault();
+  }
+
   return (
     <div>
       <h2>Login</h2>
-      <form method="post">
-        <input type="text" placeholder="Input username" />
+      <form method="post" onSubmit={handleSubmit}>
+        <input
+          type="text"
+          placeholder="Input username"
+          onChange={(event) => setUsername(event.target.value)}
+        />
         <button type="submit">Submit</button>
       </form>
     </div>
