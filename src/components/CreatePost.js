53b758ec8d1604a15ec7932d2e598329d8ef64d1
nelo <nelokwaz@gmail.com>
2021-01-09 16:03:56 +0100

    added logic to submit post

--- src/components/CreatePost.js
+++ src/components/CreatePost.js
@@ -1,16 +1,35 @@
 import React from "react";
 
 function CreatePost() {
+  const [content, setContent] = React.useState("");
+  const [image, setImage] = React.useState(null);
+
   return (
     <div>
       <h1>Create New Post</h1>
       <form action="">
-        <input type="text" placeholder="Add Post Content" />
-        <input type="file" />
+        <input
+          type="text"
+          placeholder="Add Post Content"
+          onChange={(event) => setContent(event.target.value)}
+        />
+        <input
+          type="file"
+          onChange={(event) => setImage(event.target.files[0])}
+        />
         <button type="submit">Submit Post</button>
       </form>
+      <p>{content}</p>
+      {image && (
+        <img
+          style={{ height: 100, width: 200, objectFit: "cover" }}
+          src={URL.createObjectURL(image)}
+          alt="post"
+        />
+      )}
     </div>
   );
 }
 
 export default CreatePost;
+  
