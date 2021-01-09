69e1f3c671961e2a274605feb0a5a820f2d027c8
nelo <nelokwaz@gmail.com>
2021-01-09 10:27:51 +0100

    changed the user variable to a useState

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import Login from "./components/Login";
 function App() {
-  let user = "";
+  const [user, setUser] = React.useState("");
 
   if (!user) {
     return <Login />;
