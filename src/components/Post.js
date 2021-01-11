7afe6bf0c1215ef12e03d10e2d40fcb731d6be02
nelo <nelokwaz@gmail.com>
2021-01-11 19:42:58 +0100

    retrieved the data stored in the user context inside of the Post component

--- src/components/Post.js
+++ src/components/Post.js
@@ -2,17 +2,21 @@ import React from "react";
 
 function Post({ image, content, user }) {
   return (
-    <React.Fragment>
-      {image && (
-        <img
-          style={{ height: 100, width: 200, objectFit: "cover" }}
-          alt={content}
-          src={URL.createObjectURL(image)}
-        />
+    <UserContext.Consumer>
+      {(currentUser) => (
+        <>
+          {image && (
+            <img
+              style={{ height: 100, width: 200, objectFit: "cover" }}
+              alt={content}
+              src={URL.createObjectURL(image)}
+            />
+          )}
+          <p>{content}</p>
+          <div>{user}</div>
+        </>
       )}
-      <p>{content}</p>
-      <div>{user}</div>
-    </React.Fragment>
+    </UserContext.Consumer>
   );
 }
 
