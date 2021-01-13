77a0f94454d8d7c35b9836dfa290b669c9925307
nelo <nelokwaz@gmail.com>
2021-01-13 09:20:20 +0100

    added the logic to add post to previous state in the reducer function

--- src/reducer.js
+++ src/reducer.js
@@ -1,6 +1,8 @@
 function postReducer(state, action) {
   switch (action.type) {
     case "ADD_POST": {
+      const newPost = action.payload.post;
+      return { posts: [newPost, ...state.posts] };
     }
     case "DELETE_POST": {
     }
